d27409d2496c2796e8ab0bc97444430db093a3d4
Kiel Lewis <kiellewis@Kiels-MacBook-Pro.local>
2019-01-25 11:06:23 -0500

    Project fully functional.
    Everything works.
    CSS has been altered to exeed expectations.
    Only thing left is to fill it out and remove ipsum texts.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TechDegree-Unit-6
+- made changes to font from Merriweather to Poppins
+- created .cell.shrink img.thumbnail selector to remove border and add border radius
+- background change to .portfolio-me
+- change color of .portfolio-me a
+- created a .cell.auto h5 selector to align text to center
+- color change in .portfolio-me p
--- app.js
+++ app.js
@@ -9,26 +9,34 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/static', express.static('public')); // serve static files
 app.set('view engine', 'pug');
 
+
 // routes
 app.get('/',(req, res) => { // home page
     res.render('index', { projects })
 })
 
 app.get('/about',(req, res) => { // about page
-    res.render('about')
-    
+    res.render('about')   
 })
 
 app.get('/project:id',(req, res) => { // project pages
     res.render('project', { projects, id : req.params.id })
 })
 
+app.use((req, res, next) => {
+    const err = new Error('Oops you seem to have wandered off!');
+    err.status = 404;
+    next(err); 
+})
 
+app.use((err, req, res, next) => { 
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
 
 app.listen(3000, () => {
     console.log('App running on port 3000')
 });
-
-
 
 module.exports = app;
